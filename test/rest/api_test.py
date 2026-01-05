08bd3857808d85bf4d5f6547a78d5223ff52e2c3
Javier <javifrancesm@gmail.com>
2026-01-05 13:02:53 +0100

    Update api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -55,6 +55,15 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "4.0", "ERROR ADD"
         )        
 
+    def test_api_divide_by_zero(self):
+        url = f"{BASE_URL}/calc/divide/12/0"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.NOT_ACCEPTABLE, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "Error HTTP 406", "ERROR ADD"
+        )
 
 '''
     def test_api_sqrt(self):
